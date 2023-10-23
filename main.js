b3aeb78e61eab9af3004ed20471d7d32f7ec961a
Renato <renato.f.fabri@gmail.com>
2023-10-23 18:58:18 -0300

    generate grid

--- main.js
+++ main.js
@@ -18,5 +18,19 @@ async function directionToMove() {
   }
 }
 
-const dir = await directionToMove();
-console.log(dir);
+// const dir = await directionToMove();
+// console.log(dir);
+
+function generateGrid(grid) {
+  for (let i = 0; i < 10; i++) {
+    grid[i] = [];
+    for (let j = 0; j < 10; j++) {
+      grid[i][j] = "🌲 ";
+    }
+  }
+  for (const row of grid) {
+    const rowStr = row.join("");
+    console.log(rowStr);
+  }
+}
+generateGrid();
