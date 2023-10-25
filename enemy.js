f900d3b840a7418fa068ec3a852d3fb998da9327
Renato <renato.f.fabri@gmail.com>
2023-10-25 18:14:45 -0300

    Add Player, Enemy Classes

---
+++ enemy.js
@@ -0,0 +1,9 @@
+class Enemy {
+  constructor(hp, attack, defense) {
+    this.hp = hp;
+    this.attack = attack;
+    this.defense = defense;
+  }
+  attackPlayer() {}
+  defend() {}
+}
---
+++ player.js
@@ -0,0 +1,9 @@
+class Player {
+  constructor(hp, attack, defense) {
+    this.hp = hp;
+    this.attack = attack;
+    this.defense = defense;
+  }
+  attackEnemy() {}
+  defend() {}
+}
