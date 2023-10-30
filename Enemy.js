35491015db18de6ed0f63e40fe96c2146e07eb58
Renato <renato.f.fabri@gmail.com>
2023-10-30 16:15:35 -0300

    Add Enemy class

--- Enemy.js
+++ Enemy.js
@@ -1,15 +1,37 @@
-class Enemy {
-  constructor(hp, attack, defense) {
-    this.hp = hp;
-    this.attack = attack;
-    this.defense = defense;
+import { GridItem } from "./GridItem.js";
+
+class Enemy extends GridItem {
+  #stats = {
+    name: null,
+    attack: 0,
+    defense: 0,
+    hp: 0,
+  };
+  constructor(sprite, stats) {
+    super(sprite);
+    this.type = "enemy";
+    this.#stats = stats;
+  }
+
+  getName() {
+    return this.#stats.name;
+  }
+
+  getStats() {
+    return {
+      attack: this.#stats.attack,
+      defense: this.#stats.defense,
+      hp: this.#stats.hp,
+    };
+  }
+
+  describe() {
+    const stats = this.#stats;
+    console.log(`${this.sprite} You encountered a ${stats.name}!`);
+    console.log(
+      `${stats.name}'s Stats: HP: ${stats.hp} | ATK: ${stats.attack} | DEF: ${stats.defense}`
+    );
   }
 }
 
-class Spider extends Enemy {
-  constructor(hp, attack, defense) {
-    super(hp, attack, defense);
-  }
-}
-
-export { Spider };
+export { Enemy };
