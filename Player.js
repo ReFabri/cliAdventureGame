335971f2a3a480f2839a29e8f4520186615c2d0f
Renato <renato.f.fabri@gmail.com>
2023-10-30 16:32:51 -0300

    Add Player class

--- Player.js
+++ Player.js
@@ -1,9 +1,38 @@
 class Player {
-  constructor(hp, attack, defense) {
-    this.hp = hp;
-    this.attack = attack;
-    this.defense = defense;
+  #stats = {
+    attack: 0,
+    defense: 0,
+    hp: 0,
+  };
+  constructor(name, stats) {
+    this.name = name;
+    this.#stats = stats;
   }
-  attackEnemy() {}
-  defend() {}
+
+  getStats() {
+    return {
+      attack: this.#stats.attack,
+      defense: this.#stats.defense,
+      hp: this.#stats.hp,
+    };
+  }
+
+  getName() {
+    return this.#stats.name;
+  }
+
+  addToStats(statsObject) {
+    if (statsObject.attack) this.#stats.attack += statsObject.attack;
+    if (statsObject.defense) this.#stats.defense += statsObject.defense;
+    if (statsObject.hp) this.#stats.hp += statsObject.hp;
+  }
+
+  describe() {
+    const stats = this.#stats;
+    console.log(
+      `Player Stats: HP: ${stats.hp} | ATK: ${stats.attack} | DEF: ${stats.defense}`
+    );
+  }
 }
+
+export { Player };
